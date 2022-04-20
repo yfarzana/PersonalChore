552601ecb8561ccd2e80b0db4439ba6802b31fb8
Yeasmin Farzana <yeasminf18@vt.edu>
2022-04-20 10:36:04 -0400

    initial commit

---
+++ template/script.js
@@ -0,0 +1,3 @@
+// By default all JS elements are in the window, Our browser window is called window
+//If we are calling an API and we want to just see how the data is formatted, we can console log that
+//console.log("Hello World");
